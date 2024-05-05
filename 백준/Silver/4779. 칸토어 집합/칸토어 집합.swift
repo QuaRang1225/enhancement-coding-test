6f5288a3d8fcf2c7fe66677192f8aa3e0f7f3b74
QuaRang <31721255+QuaRang1225@users.noreply.github.com>
2024-05-05 18:17:05 +0900

    [Silver III] Title: 칸토어 집합, Time: 44 ms, Memory: 81152 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.swift
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.swift
@@ -1,10 +1,8 @@
 import Foundation
-
 var inputLines: [Int] = []
 func cantor(num: Double) -> String {
     guard num != 0 else {return "-"}
     let divisible = cantor(num: num-1)
     return divisible + String(repeating: " ", count: Int(pow(3,num-1))) + divisible
 }
-
 while let line = readLine(), let num = Double(line) {  print(cantor(num: num)) }
