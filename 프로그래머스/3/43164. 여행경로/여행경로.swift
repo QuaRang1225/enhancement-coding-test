5c923ab838c580173a4e225d02126d3b5931b22f
QuaRang <31721255+QuaRang1225@users.noreply.github.com>
2024-05-27 20:20:13 +0900

    [level 3] Title: 여행경로, Time: 0.11 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+func solution(_ tickets:[[String]]) -> [String] {
+    var tickets = tickets.sorted {$0[1] < $1[1]}
+    var visited = [Bool](repeating: false, count: tickets.count)
+
+    var route = [String]()
+    
+    func dfs(_ start:String){
+        guard route.count != tickets.count else{
+            route.append(start)
+            return
+        }
+        for i in 0..<tickets.count{
+            if tickets[i][0] == start,!visited[i]{
+                visited[i] = true
+                route.append(start)
+                dfs(tickets[i][1])
+                if route.count == tickets.count + 1{ return }
+                route.removeLast()
+                visited[i] = false
+            }
+        }
+    }
+    
+    dfs("ICN")
+    return route
+}
