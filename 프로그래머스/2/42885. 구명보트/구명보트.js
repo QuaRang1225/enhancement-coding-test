06e72199ab392ce2862c53e60a08120eb1f448e5
QuaRang <31721255+QuaRang1225@users.noreply.github.com>
2024-06-08 21:36:05 +0900

    [level 2] Title: 구명보트, Time: 25.79 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,17 @@
+function solution(people, limit) {
+    people.sort((a, b) => a - b);
+    let result = 0;
+    let start = 0;
+    let end = people.length - 1;
+    
+    while (start <= end) {
+        if (people[start] + people[end] <= limit) {
+            start += 1;
+            end -= 1;
+        } else {
+            end -= 1;
+        }
+        result += 1;
+    }
+    return result;
+}
