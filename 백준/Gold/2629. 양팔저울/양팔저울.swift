392afb94e1f73cec7baa8407f71f45fce429dcf2
QuaRang <31721255+QuaRang1225@users.noreply.github.com>
2024-06-11 21:47:33 +0900

    [Gold III] Title: 양팔저울, Time: 12 ms, Memory: 69528 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.swift
+++ 백준/Gold/2629. 양팔저울/양팔저울.swift
@@ -7,32 +7,21 @@ let n = Int(readLine()!)!
     var result = [String]()
 
     func cal(_ num: Int, _ weight: Int) {
-        if num > n {
-            return
-        }
-        if dp[num][weight] {
-            return
-        }
+        if num > n || dp[num][weight]{ return }
         dp[num][weight] = true
+        
         if num < n {
             cal(num + 1, weight)
             cal(num + 1, weight + k[num])
             cal(num + 1, abs(weight - k[num]))
         }
     }
-
     cal(0, 0)
-
     for i in l {
-        if i > 30 * 500 {
+        if i > 15000 {
             result.append("N")
             continue
         }
-        if dp[n][i] {
-            result.append("Y")
-        } else {
-            result.append("N")
-        }
+        result.append(dp[n][i] ? "Y" : "N")
     }
-
     print(result.joined(separator: " "))
