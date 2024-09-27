fa32bd380bd645a2ed851397adcb600c923db13b
QuaRang <31721255+QuaRang1225@users.noreply.github.com>
2024-09-27 21:41:23 +0900

    [level 2] Title: 카펫, Time: 0.16 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ brown:Int, _ yellow:Int) -> [Int] {
+    let all = (brown + yellow)
+    var multiple = [[Int]]()
+    for i in (2..<(Int(sqrt(Double(all))) + 1)).reversed(){
+        if (i*2) + ((all/i - 2) * 2) == brown{
+            return [all/i,i]
+        }
+    }
+    return []
+}
