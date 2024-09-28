06fc9d8b03ec3a8e4203a73a52e076477e77944f
QuaRang <31721255+QuaRang1225@users.noreply.github.com>
2024-09-28 14:18:34 +0900

    [Gold V] Title: 토마토, Time: 328 ms, Memory: 125488 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.swift
+++ 백준/Gold/7576. 토마토/토마토.swift
@@ -1,48 +1,39 @@
 let input = readLine()!.split(separator: " ").compactMap{Int($0)}
-let (y, x) = (input[1], input[0])
-
-var arr = [[Int]]()
-var queue = [[Int]]()
-var front = 0
-var cnt = 0
-
-let dx = [0, 0, 1, -1]
-let dy = [1, -1, 0, 0]
-
-// 입력 받기
-(0..<y).forEach { i in
-    let line = readLine()!.split(separator: " ").compactMap { Int($0) }
-    arr.append(line)
-    queue.append(contentsOf: line.enumerated().filter { $0.element == 1 }.map { [i, $0.offset] })
-}
-
-var visited = [[Bool]](repeating: [Bool](repeating: false, count: x), count: y)
-queue.forEach { visited[$0[0]][$0[1]] = true }
-
-// BFS 탐색
-while front < queue.count {
-    let size = queue.count
-    for _ in 0..<size - front {
-        let node = queue[front]
-        front += 1
-
-        for i in 0..<4 {
-            let ny = dy[i] + node[0]
-            let nx = dx[i] + node[1]
-
-            if (0..<y) ~= ny, (0..<x) ~= nx, !visited[ny][nx], arr[ny][nx] == 0 {
-                visited[ny][nx] = true
-                queue.append([ny, nx])
-                arr[ny][nx] = 1
+    let (y,x) = (input[1],input[0])
+    
+    var arr = [[Int]]()
+    var queueA = [[Int]]()
+    var queueB = [[Int]]()
+    var cnt = 0
+    
+    let dx = [0,0,1,-1]
+    let dy = [1,-1,0,0]
+    
+    (0..<y).forEach{ i in
+        let line = readLine()!.split(separator: " ").compactMap{Int($0)}
+        arr.append(line)
+        queueA.append(contentsOf: line.enumerated().filter{$0.element == 1}.map{[i,$0.offset]})
+    }
+    var visited = [[Bool]](repeating: [Bool](repeating: false, count: x), count: y)
+    queueA.forEach{ visited[$0[0]][$0[1]] = true }
+    
+    while !queueA.isEmpty{
+        let node = queueA.removeLast()
+        for i in 0..<4{
+            let dy = dy[i] + node[0]
+            let dx = dx[i] + node[1]
+            if (0..<y)~=dy,(0..<x)~=dx,!visited[dy][dx],arr[dy][dx] == 0{
+                visited[dy][dx] = true
+                queueB.append([dy,dx])
+                arr[dy][dx] = 1
             }
         }
+        if queueA.isEmpty{
+            queueA = queueB
+            queueB = []
+            cnt += 1
+        }
     }
-    cnt += 1
-}
-
-cnt -= 1
-if arr.flatMap({ $0 }).contains(0) {
-    print(-1)
-} else {
-    print(cnt)
-}
+    cnt -= 1
+    if arr.flatMap({$0}).contains(0){print(-1)}
+    else{print(cnt)}
