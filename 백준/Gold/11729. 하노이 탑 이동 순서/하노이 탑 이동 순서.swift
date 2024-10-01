dc8e57df2f3d6925cb9d1c6b5e08932644b2e4ee
QuaRang <31721255+QuaRang1225@users.noreply.github.com>
2024-10-01 15:24:30 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 268 ms, Memory: 76980 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.swift
@@ -0,0 +1,15 @@
+let input = Int(readLine()!)!
+var arr = ""
+var cnt = 0
+func move(_ n:Int,_ from:Int,_ other:Int,_ to:Int){
+    if n == 0 {return}
+    move(n-1, from, to, other)
+    cnt += 1
+    arr += "\(from) \(to)\n"
+    move(n-1, other, from, to)
+
+}
+
+move(input,1,2,3)
+print(cnt)
+print(arr)
