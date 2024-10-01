84773ad8d7679068582dc5a4ff57b6687e427797
QuaRang <31721255+QuaRang1225@users.noreply.github.com>
2024-10-02 01:21:54 +0900

    [Silver III] Title: 바이러스, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.swift
@@ -0,0 +1,22 @@
+let n = Int(readLine()!)!
+let m = Int(readLine()!)!
+var arr = [Int:[Int]]()
+var visited = [Bool](repeating: false, count: n+1)
+var cnt = 0
+(0..<m).forEach { _ in
+                 let n = readLine()!.split(separator: " ").compactMap{Int($0)}
+                 arr[n[0],default: []].append(n[1])
+                 arr[n[1],default: []].append(n[0])
+                }
+func dfs(_ s:Int){
+    visited[s] = true
+    guard arr[s] != nil else {return}
+    for i in arr[s]!{
+        if !visited[i]{
+            cnt += 1
+            dfs(i)
+        }
+    }
+}
+dfs(1)
+print(cnt)
