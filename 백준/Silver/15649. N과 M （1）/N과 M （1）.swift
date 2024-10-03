2d45b17082a4301e673e8ec825abfd9f042c3897
QuaRang <31721255+QuaRang1225@users.noreply.github.com>
2024-10-03 15:40:07 +0900

    [Silver III] Title: N과 M (1), Time: 88 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.swift
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.swift
@@ -1,18 +1,21 @@
 let input = readLine()!.split(separator: " ").compactMap{ Int($0) }
-    let (n,m) = (input[0],input[1])
-    var arr = [Int]()
-    
-    func dfs(){
-        if arr.count == m{
-            print(arr.map{String($0)}.joined(separator: " "))
-            return
-        }
-        for i in 1...n{
-            if !arr.contains(i){
-                arr.append(i)
-                dfs()
-                arr.removeLast()
-            }
+let (n,m) = (input[0],input[1])
+var arr = [Int]()
+var visited = [Bool](repeating: false, count: n)
+
+func dfs(_ depth:Int){
+    if depth == m{
+        print(arr.map{String($0)}.joined(separator: " "))
+        return
+    }
+    for i in 0..<n{
+        if !visited[i]{
+            visited[i] = true
+            arr.append(i+1)
+            dfs(depth+1)
+            arr.removeLast()
+            visited[i] = false
         }
     }
-    dfs()
+}
+dfs(0)
