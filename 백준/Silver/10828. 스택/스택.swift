687fc05107b6553aac37a8e94c9df2e8611262bf
QuaRang <31721255+QuaRang1225@users.noreply.github.com>
2024-10-05 01:46:10 +0900

    [Silver IV] Title: 스택, Time: 24 ms, Memory: 69764 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.swift
+++ 백준/Silver/10828. 스택/스택.swift
@@ -1,46 +1,25 @@
-func stack(){
-    let stack = Stack()
-    var datas = [Int]()
-    var commands = [String]()
-    
-    let n = Int(readLine()!)!
-    for _ in 0..<n{
-        var c = readLine()!.split(separator: " ")
-        if c.count > 1{  datas.append(Int(c.removeLast())!) }
-        commands.append(String(c[0]))
-    }
-    for i in commands{
-        var d = 0
-        if i == "push"{ d = datas.removeFirst() }
-        command(i,d)
-    }
-    func command(_ c:String,_ d:Int){
-        switch c{
-        case "push": stack.push(d)
-        case "pop": print(stack.pop)
-        case "size": print(stack.size)
-        case "empty": print(stack.empty)
-        case "top": print(stack.top)
+var stack = [Int]()
+var datas = [Int]()
+var commands = [String]()
+
+let n = Int(readLine()!)!
+for _ in 0..<n{
+    var c = readLine()!.split(separator: " ")
+    if c.count > 1{  datas.append(Int(c.removeLast())!) }
+    commands.append(String(c[0]))
+}
+for i in commands{
+    var d = 0
+    if i == "push"{ d = datas.removeFirst() }
+    command(i,d)
+}
+func command(_ c:String,_ d:Int){
+    switch c{
+        case "push": stack.append(d)
+        case "pop": print(!stack.isEmpty ? stack.removeLast() : -1)
+        case "size": print(stack.count)
+        case "empty": print(stack.isEmpty ? 1 : 0)
+        case "top": print(stack.last ?? -1)
         default: break
-        }
     }
 }
-class Stack{
-    var stack = [Int]()
-    var empty:Int{
-        stack.isEmpty ? 1 : 0
-    }
-    var top:Int{
-        stack.last ?? -1
-    }
-    var size:Int{
-        stack.count
-    }
-    func push(_ num:Int){
-        stack.append(num)
-    }
-    var pop:Int{
-        !stack.isEmpty ? stack.removeLast() : -1
-    }
-}
-stack()
