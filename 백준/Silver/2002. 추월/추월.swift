40d39cf4ee7c1617bb9626f06312b92684bf1430
QuaRang <31721255+QuaRang1225@users.noreply.github.com>
2024-10-04 22:43:48 +0900

    [Silver II] Title: 추월, Time: 12 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/2002. 추월/추월.swift
+++ 백준/Silver/2002. 추월/추월.swift
@@ -1,14 +1,12 @@
 let n = Int(readLine()!)!
-var stack1 = [String]()
-var stack2 = [String]()
-for _ in 0..<n{stack1.append(readLine()!)}
-for _ in 0..<n{stack2.append(readLine()!)}
+var a = [String]()
+var b = [String]()
+for _ in 0..<n{a.append(readLine()!)}
+for _ in 0..<n{b.append(readLine()!)}
 var cnt = 0
-while !stack1.isEmpty{
-    let n = stack2.removeFirst()
-    if stack1[0] != n{
-        cnt += 1
-    }
-    stack1 = stack1.filter{ $0 != n}
+while !a.isEmpty{
+    let n = b.removeFirst()
+    if a[0] != n{ cnt += 1 }
+    a = a.filter{ $0 != n }
 }
 print(cnt)
