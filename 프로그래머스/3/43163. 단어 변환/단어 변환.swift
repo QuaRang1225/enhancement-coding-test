9e46288ce22d54889fbd9de42dcadba5267f0f67
QuaRang <31721255+QuaRang1225@users.noreply.github.com>
2025-03-19 15:22:25 +0900

    [level 3] Title: 단어 변환, Time: 1.87 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.swift
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.swift
@@ -8,15 +8,13 @@ func solution(_ begin:String, _ target:String, _ words:[String]) -> Int {
         compare.reserveCapacity(words.count)
         var step = 0
         while !queue.isEmpty {
-            let node = queue.removeLast()
+            let node = queue.removeFirst()
             guard node != target else{ return step }
-            (0...queue.count).forEach { _ in
-                words.forEach { word in
-                    let diff = zip(node, word).filter{ $0 != $1 }.count
-                    if diff == 1,!compare.contains(word){ 
-                        queue.append(word)
-                        compare.append(word) 
-                    }
+            for word in words{
+                let diff = zip(node, word).filter{ $0 != $1 }.count
+                if diff == 1,!compare.contains(word){ 
+                    queue.append(word)
+                    compare.append(word) 
                 }
             }
             step += 1
