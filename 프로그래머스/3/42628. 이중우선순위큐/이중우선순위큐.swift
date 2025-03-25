c3de068df993f39624ca1d7b62f732a8d31018f1
QuaRang <31721255+QuaRang1225@users.noreply.github.com>
2025-03-25 09:34:14 +0900

    [level 3] Title: 이중우선순위큐, Time: 780.92 ms, Memory: 27.4 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.swift
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.swift
@@ -3,27 +3,22 @@ import Foundation
 func solution(_ operations:[String]) -> [Int] {
     
     var operations = operations.map{ $0.components(separatedBy: " ")}.map{ ($0[0],Int($0[1])!)}
-    var queue = [Int]()
+    var queue = Set<Int>()
+    
     for (command,value) in operations{
         switch command{
-        case "I" : queue.append(value)
-        case "D" where value == -1 && !queue.isEmpty : queue = remove(.min,queue)
-        case "D" where value == 1 : queue = remove(.max,queue)
+        case "I" : queue.insert(value)
+        case "D" where value == -1 && !queue.isEmpty :
+            // queue.sort(by:>)
+            // queue.removeLast()
+            queue.remove(queue.min()!)
+        case "D" where value == 1 && !queue.isEmpty: 
+            // queue.sort(by:<)
+            // queue.removeLast()
+            queue.remove(queue.max()!)
         default: break
         }
     }
     guard !queue.isEmpty else { return [0,0] }
     return [queue.max()!,queue.min()!]
-}
-enum Mode{
-    case min
-    case max
-}
-func remove(_ mode:Mode,_ arr:[Int])->[Int]{
-    guard !arr.isEmpty else { return [] }
-    var arr = arr
-    let data = (mode == .min ? arr.min()! : arr.max()!)
-    let index = arr.firstIndex(of:data)!
-    arr.remove(at:index)
-    return arr
 }
